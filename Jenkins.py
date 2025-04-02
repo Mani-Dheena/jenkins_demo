a3e5b1ca34fa9511ca59276243aa3066ba23b398
unknown <manimani93444@gmail.com>
2025-04-02 14:31:04 +0530

    add the files

---
+++ Jenkins.py
@@ -0,0 +1,18 @@
+pipeline {
+  agent any
+  stages {
+    stage('version') {
+      steps {
+        bat '"C:\\Windows\\System32\\cmd.exe" /c python --version'
+      }
+    }
+    stage('hello') {
+      steps {
+        bat '"C:\\Windows\\System32\\cmd.exe" /c python test1.py %X_VALUE% %Y_VALUE%'
+      }
+    }
+  }
+}
+
+
+
---
+++ test1.py
@@ -0,0 +1,6 @@
+import sys
+print ("junkins Build using parameters ! ")
+X_VALUE = int (sys.argv[1])
+Y_VALUE = int (sys.argv[2])
+
+print ("Mutiply =",(X_VALUE * Y_VALUE))
