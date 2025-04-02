ff09c1e9a237eb8c31a668a5cbf5291afb6d0b15
unknown <manimani93444@gmail.com>
2025-04-02 14:48:05 +0530

    add new file

--- Jenkins.py
+++ Jenkins.py
@@ -6,7 +6,7 @@ pipeline {
         bat '"C:\\Windows\\System32\\cmd.exe" /c python --version'
       }
     }
-    stage('hello') {
+    stage('param') {
       steps {
         bat '"C:\\Windows\\System32\\cmd.exe" /c python test1.py %X_VALUE% %Y_VALUE%'
       }
